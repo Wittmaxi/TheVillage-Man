9a638bce770cc317c71ca01ac89ad56d7c95f7d3
wittmaxi@outlook.de <wittmaxi@outlook.de>
2018-11-19 19:41:01 +0100

    Changed allowance

--- src/manpage.cpp
+++ src/manpage.cpp
@@ -26,7 +26,7 @@ void printMan () {
     if (ENV::GET("s") != "") {
         std::string queryString = ENV::GET("s");
         std::transform(queryString.begin(), queryString.end(), queryString.begin(), tolower);
-        queryString = std::regex_replace (queryString, std::regex("([^a-z])+"), "");
+        queryString = std::regex_replace (queryString, std::regex("([^a-z\\-\\_0-9\\[\\]])+"), "");
         std::string stor = exec ("man \"" + queryString + "\"");
         if (stor == "") {
             std::cout << ConvenientText("We are sorry, but there was no manpage entry to " + queryString);
